4af36c80af5f621523f0723c2fd366ebf45b739d
Gilad Aminoff <giladaminoff@gmail.com>
2016-06-23 22:18:40 +0300

    added plane const

--- js/planes.js
+++ js/planes.js
@@ -1,2 +1,8 @@
 'use strict';
 
+// This is a constructor function
+function Plane(model, seatCount, id) {
+    this.model = model;
+    this.seatCount = seatCount;
+    this.id = (id) ? id : Plane.nextId();
+}
