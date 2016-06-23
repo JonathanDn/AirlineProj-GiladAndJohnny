9851e0d0a900edd183b25cedbd05b47256b6ce69
Gilad Aminoff <giladaminoff@gmail.com>
2016-06-23 22:51:16 +0300

    asd

--- js/Plane.class.js
+++ js/Plane.class.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const KEY_PLANES = 'planes';
-
 // This is a constructor function
 function Plane(model, seatCount, id) {
     this.model = model;
@@ -13,68 +11,68 @@ function Plane(model, seatCount, id) {
 
 Plane.nextId = function () {
     let result = 1;
-    let jsonPlanes = Plane.loadJSONFromStorage();
-    if (jsonPlanes.length) result = jsonPlanes[jsonPlanes.length - 1].id + 1;
+    let jsonPassengers = Plane.loadJSONFromStorage();
+    if (jsonPassengers.length) result = jsonPassengers[jsonPassengers.length - 1].id + 1;
     return result;
 }
 
 Plane.findById = function (pId) {
     let result = null;
-    let planes = Plane.query()
+    let passengers = Plane.query()
         .filter(p => p.id === pId);
-    if (planes.length) result = planes[0];
+    if (passengers.length) result = passengers[0];
     return result;
 }
 
 Plane.loadJSONFromStorage = function () {
-    let planes = getFromStorage(KEY_PLANES);
-    if (!planes) planes = [];
-    return planes;
+    let passengers = getFromStorage(KEY_PASSENGERS);
+    if (!passengers) passengers = [];
+    return passengers;
 }
 
 
 Plane.query = function () {
 
-    if (Plane.planes) return Plane.planes;
-    let jsonPlanes = Plane.loadJSONFromStorage();
+    if (Plane.passengers) return Passenger.passengers;
+    let jsonPassengers = Plane.loadJSONFromStorage();
 
-    Plane.planes = jsonPlanes.map(jsonPlane => {
-        return new Plane(jsonPlane.name, jsonPlane.birthdate, jsonPlane.id);
+    Plane.passengers = jsonPassengers.map(jsonPassenger => {
+        return new Plane(jsonPassenger.name, jsonPassenger.birthdate, jsonPassenger.id);
     })
 
-    return Plane.planes;
+    return Plane.passengers;
 }
 
 Plane.save = function (formObj) {
-    let planes = Plane.query();
-    let plane;
+    let passengers = Plane.query();
+    let passenger;
     if (formObj.pid) {
-        plane = Plane.findById(+formObj.pid);
-        plane.name = formObj.pname;
-        plane.birthdate = new Date(formObj.pdate);
+        passenger = Plane.findById(+formObj.pid);
+        passenger.name = formObj.pname;
+        passenger.birthdate = new Date(formObj.pdate);
     } else {
-        plane = new Plane(formObj.pname, formObj.pdate);
-        planes.push(plane);
+        passenger = new Plane(formObj.pname, formObj.pdate);
+        passengers.push(passenger);
     }
-    Plane.planes = planes;
-    saveToStorage(KEY_PLANES, planes);
+    Plane.passengers = passengers;
+    saveToStorage(KEY_PASSENGERS, passengers);
 }
 
 
 Plane.remove = function (pId, event) {
     event.stopPropagation();
-    let planes = Plane.query();
-    planes = planes.filter(p => p.id !== pId)
-    saveToStorage(KEY_PLANES, planes);
-    Plane.planes = planes;
+    let passengers = Plane.query();
+    passengers = passengers.filter(p => p.id !== pId)
+    saveToStorage(KEY_PASSENGERS, passengers);
+    Plane.passengers = passengers;
     Plane.render();
 }
 
 
 Plane.render = function () {
 
-    let planes = Plane.query();
-    var strHtml = planes.map(p => {
+    let passengers = Plane.query();
+    var strHtml = passengers.map(p => {
         return `<tr onclick="Plane.select(${p.id}, this)">
             <td>${p.id}</td>
             <td>${p.name}</td>
@@ -86,14 +84,14 @@ Plane.render = function () {
                 <button class="btn btn-danger" onclick="Plane.remove(${p.id}, event)">
                     <i class="glyphicon glyphicon-trash"></i>
                 </button>
-                 <button class="btn btn-info" onclick="Plane.editPlane(${p.id}, event)">
+                 <button class="btn btn-info" onclick="Plane.editPassenger(${p.id}, event)">
                     <i class="glyphicon glyphicon-edit"></i>
                 </button>
             </td>
         </tr>`
 
     }).join(' ');
-    $('.tblPlanes').html(strHtml);
+    $('.tblPassengers').html(strHtml);
 }
 
 Plane.select = function (pId, elRow) {
@@ -110,22 +108,22 @@ Plane.savePlane = function () {
 
     Plane.save(formObj);
     Plane.render();
-    $('#modalPlane').modal('hide');
+    $('#modalPassenger').modal('hide');
 }
 
 
 Plane.editPlane = function (pId, event) {
     if (event) event.stopPropagation();
     if (pId) {
-        let plane = Plane.findById(pId);
-        $('#pid').val(plane.id);
-        $('#pname').val(plane.name);
-        $('#pdate').val(moment(plane.birthdate).format('YYYY-MM-DD'));
+        let passenger = Plane.findById(pId);
+        $('#pid').val(passenger.id);
+        $('#pname').val(passenger.name);
+        $('#pdate').val(moment(passenger.birthdate).format('YYYY-MM-DD'));
     } else {
         $('#pid').val('');
         $('#pname').val('');
         $('#pdate').val('');
     }
 
-    $('#modalPlane').modal('show');
+    $('#modalPassenger').modal('show');
 }
